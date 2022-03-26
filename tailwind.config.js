bad4dca3b17fd4c0eff35711697d5752b4c4e413
Piyush Pradhan <piyushpradhan3.14@gmail.com>
2022-03-26 09:55:02 +0530

    add configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        secondary: "#C6E1F0",
+        lighter: "#EFF6FF",
+        light: "#A2DBFA",
+        primary: "#1B87C4",
+        bg: "#ffffff",
+      },
+      dropShadow: {
+        secondary: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+      },
+      boxShadow: {
+        secondary: "4px 4px 20px rgba(0, 0, 0, 0.25)",
+      },
+    },
+    letterSpacing: {
+      widest2: "1.2rem",
+    },
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
